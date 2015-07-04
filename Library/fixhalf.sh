14de2b3aa9bdbce41d2f2c7031b2f024f62d8814
Jay Freeman (saurik) <saurik@saurik.com>
2015-07-04 11:14:05 -0700

    I seriously copy/pasted %@ into a shell script :/.

--- Library/fixhalf.sh
+++ Library/fixhalf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -f /var/lib/dpkg/info/%@.prerm
-rm -f /var/lib/dpkg/info/%@.postrm
-rm -f /var/lib/dpkg/info/%@.preinst
-rm -f /var/lib/dpkg/info/%@.postinst
-rm -f /var/lib/dpkg/info/%@.extrainst_
+rm -f /var/lib/dpkg/info/"$1".prerm
+rm -f /var/lib/dpkg/info/"$1".postrm
+rm -f /var/lib/dpkg/info/"$1".preinst
+rm -f /var/lib/dpkg/info/"$1".postinst
+rm -f /var/lib/dpkg/info/"$1".extrainst_
