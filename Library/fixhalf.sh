7cf0c7b3cdd2c1e190fcbd8d05ac5775d935b687
Jay Freeman (saurik) <saurik@saurik.com>
2015-07-05 00:57:41 -0700

    No: inline implementation is *equally* invalid ;P.

--- Library/fixhalf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -f /var/lib/dpkg/info/"$1".prerm
-rm -f /var/lib/dpkg/info/"$1".postrm
-rm -f /var/lib/dpkg/info/"$1".preinst
-rm -f /var/lib/dpkg/info/"$1".postinst
-rm -f /var/lib/dpkg/info/"$1".extrainst_
