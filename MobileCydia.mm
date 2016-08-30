929dab27d82d887d26fda82a2f89a11d7d0df004
John Coates <acapulco1988@gmail.com>
2016-08-30 02:59:07 -0700

    moved Diversion into its own file

--- MobileCydia.mm
+++ MobileCydia.mm
@@ -154,64 +154,7 @@ extern "C" {
 #import "Section.h"
 #import "Logging.hpp"
 #import "Database.h"
-
-/* Database Implementation {{{ */
-
-/* }}} */
-
-static _H<NSMutableSet> Diversions_;
-
-@interface Diversion : NSObject {
-    RegEx pattern_;
-    _H<NSString> key_;
-    _H<NSString> format_;
-}
-
-@end
-
-@implementation Diversion
-
-- (id) initWithFrom:(NSString *)from to:(NSString *)to {
-    if ((self = [super init]) != nil) {
-        pattern_ = [from UTF8String];
-        key_ = from;
-        format_ = to;
-    } return self;
-}
-
-- (NSString *) divert:(NSString *)url {
-    return !pattern_(url) ? nil : pattern_->*format_;
-}
-
-+ (NSURL *) divertURL:(NSURL *)url {
-  divert:
-    NSString *href([url absoluteString]);
-
-    for (Diversion *diversion in (id) Diversions_)
-        if (NSString *diverted = [diversion divert:href]) {
-#if !ForRelease
-            NSLog(@"div: %@", diverted);
-#endif
-            url = [NSURL URLWithString:diverted];
-            goto divert;
-        }
-
-    return url;
-}
-
-- (NSString *) key {
-    return key_;
-}
-
-- (NSUInteger) hash {
-    return [key_ hash];
-}
-
-- (BOOL) isEqual:(Diversion *)object {
-    return self == object || ([self class] == [object class] && [key_ isEqual:[object key]]);
-}
-
-@end
+#import "Diversion.h"
 
 @interface CydiaObject : NSObject {
     _H<CyteWebViewController> indirect_;
