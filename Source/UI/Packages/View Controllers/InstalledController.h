766aeafb47fdaae8fce6cd44035ab827f2d7ebd8
John Coates <acapulco1988@gmail.com>
2016-08-30 13:21:40 -0700

    moved InstalledController to its own file

---
+++ Source/UI/Packages/View Controllers/InstalledController.h
@@ -0,0 +1,19 @@
+//
+//  InstalledController.h
+//  Cydia
+//
+//  Created on 8/30/16.
+//
+
+#import "FilteredPackageListController.h"
+
+@class Database;
+
+@interface InstalledController : FilteredPackageListController {
+    bool sectioned_;
+}
+
+- (id) initWithDatabase:(Database *)database;
+- (void) queueStatusDidChange;
+
+@end
