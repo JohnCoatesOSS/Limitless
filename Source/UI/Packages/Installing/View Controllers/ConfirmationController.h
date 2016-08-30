ebf1abe4c1a09242e8869ff9b3f891f577c47f56
John Coates <acapulco1988@gmail.com>
2016-08-30 09:45:51 -0700

    moved ConfirmationController into its own file

---
+++ Source/UI/Packages/Installing/View Controllers/ConfirmationController.h
@@ -0,0 +1,31 @@
+//
+//  ConfirmationController.h
+//  Cydia
+//
+//  Created on 8/30/16.
+//
+
+#import "CydiaWebViewController.h"
+#import "Database.h"
+
+@protocol ConfirmationControllerDelegate
+- (void) cancelAndClear:(bool)clear;
+- (void) confirmWithNavigationController:(UINavigationController *)navigation;
+- (void) queue;
+@end
+
+@interface ConfirmationController : CydiaWebViewController {
+    _transient Database *database_;
+    
+    _H<UIAlertView> essential_;
+    
+    _H<NSDictionary> changes_;
+    _H<NSMutableArray> issues_;
+    _H<NSDictionary> sizes_;
+    
+    BOOL substrate_;
+}
+
+- (id) initWithDatabase:(Database *)database;
+
+@end
