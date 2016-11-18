408e703a936b4cb860027f809fa5bf9955491426
John Coates <acapulco1988@gmail.com>
2016-11-17 21:19:39 -0800

    updated flags

--- Source/Globals/Flags.h
+++ Source/Globals/Flags.h
@@ -5,12 +5,12 @@
 //  Created on 8/29/16.
 //
 
-#define ForRelease 1
-#define TraceLogging (1 && !ForRelease)
+#define ForRelease 0
+#define TraceLogging (0 && !ForRelease)
 #define HistogramInsertionSort (0 && !ForRelease)
 #define ProfileTimes (0 && !ForRelease)
-#define ForSaurik (0 && !ForRelease)
-#define LogBrowser (0 && !ForRelease)
+#define ForSaurik (1 && !ForRelease)
+#define LogBrowser (1 && !ForRelease)
 #define TrackResize (0 && !ForRelease)
 #define ManualRefresh (1 && !ForRelease)
 #define ShowInternals (0 && !ForRelease)
