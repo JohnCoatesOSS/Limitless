87ecb9208c52811f80c86f6f186907ce371e46fc
John Coates <acapulco1988@gmail.com>
2016-11-29 02:09:55 -0800

    added push_macro to macOSToiOSBridge
    TODO: pop macros after WebKit imports

--- External/Headers/Compatibility/macOSToiOSBridge.h
+++ External/Headers/Compatibility/macOSToiOSBridge.h
@@ -1,12 +1,18 @@
 #import <UIKit/UIKit.h>
+// Define trickery to make classses defines for macOS available to iOS
 
+#pragma push_macro("NS_CLASS_AVAILABLE_MAC")
 #undef NS_CLASS_AVAILABLE_MAC
 #define NS_CLASS_AVAILABLE_MAC(_mac) NS_CLASS_AVAILABLE(_mac, 2_0)
+
+#pragma push_macro("NS_AVAILABLE_MAC")
 #undef NS_AVAILABLE_MAC
 #define NS_AVAILABLE_MAC(_mac) CF_AVAILABLE(_mac, 2_0)
+
+#pragma push_macro("NS_ENUM_AVAILABLE_MAC")
 #undef NS_ENUM_AVAILABLE_MAC
 #define NS_ENUM_AVAILABLE_MAC(_mac) CF_ENUM_AVAILABLE(_mac, 2_0)
-// Define trickery from macOS to iOS
+
 #define NSView UIView
 #define NSWindow UIWindow
 #define NSImage UIImage
