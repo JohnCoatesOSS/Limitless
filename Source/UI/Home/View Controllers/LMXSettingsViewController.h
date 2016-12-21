7c8b62084b2aec8b2f2d01d28230f780d44abe33
John Coates <acapulco1988@gmail.com>
2016-12-21 15:55:42 -0800

    Cleaned up header

--- Source/UI/Home/View Controllers/LMXSettingsViewController.h
+++ Source/UI/Home/View Controllers/LMXSettingsViewController.h
@@ -7,27 +7,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LMXSettingsViewController : UIViewController
-<UITableViewDataSource, UITableViewDelegate>
-
-@property (nonatomic, strong) UITableView *table;
-
-@property (nonatomic) NSInteger *page;
-@property (nonatomic, strong) UITableViewCell *defaultPageCell;
-
-@property (nonatomic, strong) UISwitch *rotationSwitch;
-@property (nonatomic, strong) UITableViewCell *rotationCell;
-
-@property (nonatomic, strong) UISwitch *nightModeSwitch;
-@property (nonatomic, strong) UITableViewCell *nightModeCell;
-
-@property (nonatomic, strong) UISwitch *autoRefreshSwitch;
-@property (nonatomic, strong) UITableViewCell *autoRefreshCell;
-
-@property (nonatomic, strong) UISwitch *timeoutSwitch;
-@property (nonatomic, strong) UITableViewCell *timeoutCell;
-
-@property (nonatomic, strong) UITextField *customTimeout;
-@property (nonatomic, strong) UITableViewCell *customTimeoutCell;
+@interface LMXSettingsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
 @end
