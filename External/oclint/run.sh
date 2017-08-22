6141e690c1ff7e701666add659f4631eb6d87182
John Coates <john@johncoates.me>
2017-08-21 19:17:15 -0700

    OCLint script now writes lintbuild.log

--- External/oclint/run.sh
+++ External/oclint/run.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR=$DIR/../../
 cd $PROJECT_DIR
-$DIR/bin/oclint-json-compilation-database -v -- -report-type xcode -R=$DIR/lib/oclint/customRules
+$DIR/bin/oclint-json-compilation-database -- -report-type xcode -R=$DIR/lib/oclint/rules -R=$DIR/lib/oclint/customRules
